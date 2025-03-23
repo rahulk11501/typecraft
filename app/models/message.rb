ef4a5740ceb1e88edcca63a841e22576308267f1
rahulk11501 <rahulkumar11501@gmail.com>
2025-03-23 22:39:21 +0000

    Adding message broadcast

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,15 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :task
+
+  after_create :broadcast_message
+
+  def broadcast_message
+    broadcast_append_to(
+      [ task, "messages" ], # This is channel name
+      target: "messages", # This is target id of the div
+      partial: "messages/message",
+      locals: { message: self }
+    )
+  end
 end
